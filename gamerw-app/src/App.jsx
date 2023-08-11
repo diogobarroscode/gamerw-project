d293625e7e7eaffee824636de733f934b6dadb3d
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-08-10 23:33:47 -0300

    config routes from project

--- gamerw-app/src/App.jsx
+++ gamerw-app/src/App.jsx
@@ -1,11 +1,13 @@
+import { Outlet } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1>HEY THERE</h1>
-    </>
+    <div className="App">
+      <Outlet />
+    </div>
   )
 }
 
---
+++ gamerw-app/src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <><h1>HEY THERE</h1></>
+  )
+}
+
+export default Home;
