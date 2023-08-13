028fb23ee9a20e8774d236abfcd341a993e767d5
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-08-13 02:03:35 -0300

    loading Spinner

--- gamerw-app/src/pages/home/Home.jsx
+++ gamerw-app/src/pages/home/Home.jsx
@@ -1,24 +1,33 @@
 import { useState, useEffect } from 'react';
 import { getPopularGames } from '../../services/rawgService';
-
+import Loading from '../../components/loading/Loading';
 import styles from './Home.module.css';
 import GameSlider from '../../components/gameSlider/GameSlider';
 
 
-const popularGames = await getPopularGames();
-console.log(popularGames);
+// const popularGames = await getPopularGames();
+// console.log(popularGames);
 
 const Home = () => {
 
+  const [loading, setLoading] = useState(true);
   const [popularGames, setPopularGames] = useState([]);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 500);
+  // }, []);
 
   useEffect(() =>{
     async function fetchPopularGames() {
       try {
         const games = await getPopularGames();
         setPopularGames(games);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching popular games:', error);
+        setLoading(false);
       }
     }
 
@@ -26,10 +35,18 @@ const Home = () => {
   }, [])
 
   return (
-    <main className={styles.main}>
-      <h2>Mais Populares em 2023</h2>
-      <GameSlider games={popularGames} />
-    </main>
+
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+       <main className={styles.main}>
+         <h2>Mais Populares em 2023</h2>
+         <GameSlider games={popularGames} />
+       </main>
+      )}
+    </>
+    
   )
 }
 
